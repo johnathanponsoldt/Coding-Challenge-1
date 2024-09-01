8da7a6be139317766964ea6e458e82210032388c
johnathanponsoldt <jponsoldt772@gmail.com>
2024-09-01 17:23:35 -0400

    Task 3 - Financial Calculations

--- cc_1.js
+++ cc_1.js
@@ -19,3 +19,19 @@ const orderDetails = {
 }
 
 console.log (orderItems, orderdetails);
+
+
+// Task 3: Assignment Operators
+
+let accountBalance = 500.00;
+console.log (accountBalance, totalAmount500(accountBalance));
+accountBalance +=100;
+console.log (accountBalance, totalAmount600(accountBalance));
+accountBalance -=50;
+console.log (accountBalance, totalAmount550(accountBalance));
+accountBalance *=1.2;
+console.log (accountBalance,totalAmount660(accountBalance));
+accountBalance /=2;
+console.log (accountBalance, totalAmount330(accountBalance));
+accountBalance %=200;
+console.log (accountBalance, totalAmount660(accountBalance));
