e3bf5f8e2f2ca98510bec18ac9994be1e42ada10
johnathanponsoldt <jponsoldt772@gmail.com>
2024-09-01 16:42:21 -0400

    Task 1 - Employee Information

---
+++ cc_1.js
@@ -0,0 +1,10 @@
+// Task 1: Variables and Data Types
+
+let customerName = "Lebron James";
+const customerID = 23;
+var isPremiumMember = false;
+
+console.log(customerName, typeof(customerName));
+console.log(customerID, typeof(customerID));
+console.log(isPremiumMember, typeof(isPremiumMember));
+
